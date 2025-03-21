a1675ee8a5c9a71dff77c4fb3f062c28e50bcd1d
tuanmcwar <tuanntch1@gmail.com>
2025-03-21 15:43:41 +0700

    1cc

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -165,9 +165,9 @@ export const processTokensProfile = async (mappingData) => {
                 ${generateMessageBoot(item)}
                 ${generateMessageGtScore(item.gecko?.data?.attributes?.gt_score)}
                 `;
-                console.log(message);
-                // await sendMessageToAllChats(message);
-                // await new TokenModel({ key: tokenKey, data: newTokenData }).save();
+              
+                await sendMessageToAllChats(message);
+                await new TokenModel({ key: tokenKey, data: newTokenData }).save();
             }
         } else if (!isEqual(existingTokenData.data, newTokenData)) {
             existingTokenData.data = newTokenData;
